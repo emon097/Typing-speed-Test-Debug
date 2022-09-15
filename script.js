382891070a85f2706a13891320d7f83139dd6cab
emon097 <mdemonhossain9527@gmail.com>
2022-09-15 12:26:49 +0600

    responsivemodal

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ const gameOver = () => {
   // the current time is the finish time
   // so total time taken is current time - start time
   const finishTime = new Date().getTime();
-  const timeTaken = (finishTime - startTime) / 3000;
+  const timeTaken = parseInt((finishTime - startTime) / 3000);
 
   // show result modal
   resultModal.innerHTML = "";
@@ -82,11 +82,14 @@ const gameOver = () => {
   // make it inactive
   display.classList.add("inactive");
   // show result
+  resultModal.classList.add("centerss");
   resultModal.innerHTML += `
+  <div class="center"  >
     <h1>Finished!</h1>
     <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
+    </div>
   `;
 
   addHistory(questionText, timeTaken, errorCount);
